333ee060ecda0d3389b2524aa4a378d838b470dd
nhk <nhk@yanolja.com>
2021-05-17 18:36:36 +0900

    Determine Request Condition: Cookie : prepare
    - change to menifest : V2 to V3
    - display current URL to input form

---
+++ sap_remote_work/popup.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input");
+
+// The async IIFE is necessary because Chrome <89 does not support top level await.
+(async function initPopupWindow() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (tab?.url) {
+    try {
+      let url = new URL(tab.url);
+      input.value = url.hostname;
+    } catch {}
+  }
+
+  input.focus();
+})();
