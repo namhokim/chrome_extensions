e30f2ca9520cbdb0201ef0f55a85e262090844ff
namo <nhkimnamo@gmail.com>
2021-05-17 19:18:12 +0900

    Determine Request Condition: Cookie - JSESSIONID

--- sap_remote_work/popup.js
+++ sap_remote_work/popup.js
@@ -2,14 +2,26 @@ const input = document.getElementById("input");
 
 // The async IIFE is necessary because Chrome <89 does not support top level await.
 (async function initPopupWindow() {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-  if (tab?.url) {
-    try {
-      let url = new URL(tab.url);
-      input.value = url.hostname;
-    } catch {}
-  }
+    if (tab?.url) {
+        try {
+            let url = new URL(tab.url);
+            let domain = url.hostname;
 
-  input.focus();
+            const cookies = await chrome.cookies.getAll({domain});
+
+            if (cookies.length === 0) {
+                input.value = "Maybe not SuccessFactor..";
+            } else {
+                let jsessionIdObj = cookies.filter(obj => obj.name === 'JSESSIONID');
+                if (jsessionIdObj !== undefined) {
+                    input.value = jsessionIdObj[0].value;
+                } else {
+                    input.value = "Maybe not SuccessFactor.";
+                }
+            }
+        } catch {
+        }
+    }
 })();
