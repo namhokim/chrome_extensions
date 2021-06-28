c286217bfd335ea35783c89a22e6b1fd8b1e2b56
namo <nhkimnamo@gmail.com>
2021-06-28 15:40:33 +0900

    Display default date by today
    - add response of ajax
    - display default date by today

--- sap_remote_work/popup.js
+++ sap_remote_work/popup.js
@@ -5,50 +5,44 @@ const dateStart = document.getElementById("dateStart");
 const message = document.getElementById("message");
 const controlRow = document.getElementById("control-row");
 
-var sid = '';
 var akey = '';
 
 // The async IIFE is necessary because Chrome <89 does not support top level await.
 (async function initPopupWindow() {
+    initDatePicker();
+
     let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
     if (tab?.url) {
-        try {
-            let url = new URL(tab.url);
-            let domain = url.hostname;
+        let url = new URL(tab.url);
+        let domain = url.hostname;
 
-            const cookies = await chrome.cookies.getAll({domain});
+        const cookies = await chrome.cookies.getAll({domain});
+        if (cookies.length === 0) {
+            setMessage("Maybe not SuccessFactor..")
+        } else {
+            let jsessionIdObj = cookies.filter(obj => obj.name === 'JSESSIONID');
+            if (jsessionIdObj !== undefined && jsessionIdObj.length > 0) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === xhr.DONE) {
+                        if (xhr.status === 200) {
+                            const regex = /var ajaxSecKey="(.*)";var/g;
+                            let result = regex.exec(xhr.responseText);
 
-            if (cookies.length === 0) {
-                message.textContent = "Maybe not SuccessFactor..";
-            } else {
-                let jsessionIdObj = cookies.filter(obj => obj.name === 'JSESSIONID');
-                if (jsessionIdObj !== undefined) {
-                    var xhr = new XMLHttpRequest();
-                    xhr.onreadystatechange = function () {
-                        if (xhr.readyState === xhr.DONE) {
-                            if (xhr.status === 200) {
-                                const regex = /var ajaxSecKey="(.*)";var/g;
-                                let result = regex.exec(xhr.responseText);
+                            akey = result[1];
 
-                                sid = jsessionIdObj[0].value;
-                                akey = result[1];
-
-                                setMessage("Ready for work!");
-                                controlRow.hidden = false;
-                            } else {
-                                consol.error(xhr.reponseText);
-                            }
+                            setMessage("Ready for work!");
+                            controlRow.hidden = false;
+                        } else {
+                            consol.error(xhr.reponseText);
                         }
-                    };
-                    xhr.open('GET', 'https://hcm44.sapsf.com/sf/start/');
-                    xhr.send();
-                } else {
-                    message.textContent = "Maybe not SuccessFactor.";
-                }
+                    }
+                };
+                xhr.open('GET', 'https://hcm44.sapsf.com/sf/start/');
+                xhr.send();
+            } else {
+                setMessage("Maybe not SuccessFactor.");
             }
-        } catch {
-            message.textContent = "Maybe error.";
         }
     }
 })();
@@ -66,7 +60,6 @@ async function handleFormSubmit(event) {
             end: dateStart.value
         },
         type: 'RemoteWork2-1',
-        sessionId: sid,
         ajaxKey: akey
     };
 
@@ -75,23 +68,38 @@ async function handleFormSubmit(event) {
     });
 }
 
-function requestRemoteWork(param, callback) {
+function initDatePicker() {
+    dateStart.value = getToday();
+}
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === xhr.DONE) {
-            callback(xhr.reponseText);
-        }
-    };
+function getToday(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
 
-    // xhr.open('POST', 'https://hcm44.sapsf.com/xi/ajax/remoting/call/plaincall/tlmTime0ffControllerProxy.createAbsence.dwr');
-    xhr.open('POST', 'http://localhost/');
+    return year + "-" + month + "-" + day;
+}
 
-    xhr.setRequestHeader('Cookie', `JSESSIONID=;${param.sessionId}`);
-    xhr.withCredentials = true;
-    xhr.setRequestHeader('x-ajax-token', param.ajaxKey);
+function getTitle() {
+    chrome.storage.local.get(['ajaxKey', 'startDate', 'endDate', 'remoteType'], function (result) {
 
-    let data = `
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === xhr.DONE ) {
+                if (xhr.response.indexOf('success') === -1) {
+                    console.log(xhr.response);
+                    alert('failed. (see console.log)');
+                } else {
+                    alert('success.');
+                }
+            }
+        };
+
+        xhr.open('POST', 'https://hcm44.sapsf.com/xi/ajax/remoting/call/plaincall/tlmTime0ffControllerProxy.createAbsence.dwr');
+        xhr.setRequestHeader('x-ajax-token', result['ajaxKey']);
+
+        let data = `
 callCount=1
 page=/xi/ui/ect/pages/absence/timeOff.xhtml?_s.crb=SBCR3KQDbJx6WJsM5NEv3B7mlUdWI8tWZk9exA2NHO0%3d
 httpSessionId=
@@ -100,9 +108,9 @@ c0-scriptName=tlmTimeOffControllerProxy
 c0-methodName=createAbsence
 c0-id=0
 c0-e1=string:10218
-c0-e2=string:RemoteWork2-1
-c0-e3=string:${param.date.start}
-c0-e4=string:${param.date.end}
+c0-e2=string:${result['remoteType']}
+c0-e3=string:${result['startDate']}
+c0-e4=string:${result['endDate']}
 c0-e5=null:null
 c0-e6=null:null
 c0-e7=string:
@@ -124,9 +132,29 @@ c0-e22=null:null
 c0-e23=boolean:false
 c0-param0=Object_Object:{userId:reference:c0-e1, timeTypeCode:reference:c0-e2, startDate:reference:c0-e3, endDate:reference:c0-e4, startTime:reference:c0-e5, endTime:reference:c0-e6, comment:reference:c0-e7, customFieldValues:reference:c0-e8, fraction:reference:c0-e15, undeterminedEndDate:reference:c0-e16, externalCode:reference:c0-e17, expReturn:reference:c0-e18, actReturn:reference:c0-e19, countryExtension:reference:c0-e20, transactionType:reference:c0-e21, linkAbsenceBean:reference:c0-e22, essLegacyMode:reference:c0-e23}
 batchId=154`;
+        xhr.send(data);
+    });
+}
 
-    setMessage(data);
-    xhr.send(data);
+function requestRemoteWork(param, callback) {
+
+    chrome.storage.local.set({
+        ajaxKey: param.ajaxKey,
+        startDate: param.date.start,
+        endDate: param.date.end,
+        remoteType: param.type
+    }, function () {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.scripting.executeScript(
+                {
+                    target: {tabId: tabs[0].id},
+                    function: getTitle,
+                },
+                (injectionResults) => {
+                    callback('Triggered!');
+                });
+        });
+    });
 }
 
 function setMessage(str) {
